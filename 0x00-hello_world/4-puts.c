0634df46088cd1fac34631e512371d0ff9b29755
Adeoye oluwatosin <lawlantosin@gmail.com>
2022-09-08 14:12:05 -0700

    second commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase
+ * Return: 0 if exited properly
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
